1547abdc0c644610142b5c4714a99a473258321a
dclerici <dclerici77@gmail.com>
2024-02-06 01:01:55 -0500

    new header

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -8,13 +8,16 @@ const HeroSection = () => {
     <section className="mx-auto mb-48 grid max-w-screen-xl gap-12 px-4 pt-[25vh] md:grid-cols-2 md:gap-0 md:px-8 lg:px-16">
       <div className="order-2 md:order-1">
         {/* make top tracks top artists and top genres align on the left */}
-        <h1 className="xs:text-3xl mb-16 flex min-w-max justify-center gap-2 text-2xl font-bold sm:text-4xl md:justify-normal md:gap-3 lg:text-5xl xl:text-6xl">
-          Your favorite
-          <span className="flex flex-col">
-            tracks <br />
-            artists <br /> genres
-          </span>
+        <h1 className="xs:text-3xl mb-4 text-center text-2xl font-bold sm:text-4xl md:text-left lg:text-5xl xl:text-6xl">
+          Spotify stats
         </h1>
+        <h2 className="xs:text-xl mb-16 flex min-w-max justify-center gap-2 text-lg font-semibold text-main/65 sm:text-2xl md:justify-normal lg:text-3xl">
+          Your top
+          <span className="flex flex-col text-main">
+            tracks. <br />
+            artists. <br /> genres.
+          </span>
+        </h2>
         <div className="flex justify-center">
           {cookies().get("refresh_token") ? (
             <Link
